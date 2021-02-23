6d665168fc007a56cb5753f819a5b8b7f533c4f3
Maxim Dodon <maximdodon@maxims-mbp.home>
2021-02-23 22:26:55 +0000

    hooks

---
+++ src/hooks/products/index.ts
@@ -0,0 +1,24 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Product } from "../../models/producets";
+
+import { basketItemsSelector, clearBasket, moveProductToBasket, restoreProductFromBasket } from "../../state/products";
+
+export const useBasket = () => {
+  const basketItems = useSelector(basketItemsSelector);
+  const dispatch = useDispatch();
+  
+  const move = useCallback((product: Product) => dispatch(moveProductToBasket(product)), [dispatch]);
+
+  const restore = useCallback((product: Product) => dispatch(restoreProductFromBasket(product)), [dispatch]);
+
+  const clear = useCallback(() => dispatch(dispatch(clearBasket())), [dispatch]);
+
+  return {
+    basketItems,
+    moveToBasket: move,
+    restoreFromBasket: restore,
+    clearBasket: clear
+  };
+};
