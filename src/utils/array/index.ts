8d622c23a4f081a691576f1641699a9f3fdf9f2c
Maxim Dodon <maximdodon@maxims-mbp.home>
2021-02-24 15:36:14 +0000

    fixed bug with remove not deleting first array item

--- src/utils/array/index.ts
+++ src/utils/array/index.ts
@@ -3,5 +3,5 @@ type Predicate<T> = (item: T) => void;
 export const remove = <T>(array: T[], predicate: Predicate<T>) => {
   const index = array.findIndex(predicate);
 
-  if (index !== 0) array.splice(index, 1);
+  if (index > -1) array.splice(index, 1);
 };
