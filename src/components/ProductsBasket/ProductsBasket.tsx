07937e9ddb9757481c01f355c9d91fe111a7d0c6
Maxim Dodon <maximdodon@maxims-mbp.home>
2021-02-24 15:35:07 +0000

    Empty basket component

--- src/components/ProductsBasket/ProductsBasket.tsx
+++ src/components/ProductsBasket/ProductsBasket.tsx
@@ -1,39 +1,117 @@
 import React, { forwardRef } from "react";
 
-import { useSelector } from "react-redux";
-import { productsListSelector } from "../../state/products";
-
-import { Dialog, DialogActions, DialogContent, DialogTitle, Button, DialogProps, Fade, makeStyles } from "@material-ui/core";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+  DialogProps,
+  Fade,
+  Divider,
+  makeStyles,
+  Typography,
+  IconButton,
+  Box,
+  Grid
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import ListAltIcon from '@material-ui/icons/ListAlt';
 import { ProductsGrid } from "../ProductsGrid";
+import BasketItem from "./BasketItem";
 
 import { useBasket } from "../../hooks";
 
-const useStyles = makeStyles(({ palette }) => ({
+type ProductsBasketProps = Omit<DialogProps, "onClose"> & {
+  onClose: () => void
+};
+
+const useStyles = makeStyles(({ palette, spacing }) => ({
+  paper: {
+    maxWidth: "unset"
+  },
+  content: {
+    width: 800,
+    height: 400
+  },
   clearButton: {
     color: palette.error.main
+  },
+  closeButton: {
+    position: 'absolute',
+    right: spacing(1),
+    top: spacing(1),
+    color: palette.grey[500],
+  },
+  submitButton: {
+    marginLeft: 10
+  },
+  emptyBasketIcon: {
+    textAlign: "center",
+    "& svg": {
+      height: "2em",
+      width: "2em"
+    }
   }
 }));
 
-const ProductsBasket = forwardRef<HTMLDivElement, DialogProps>((props, ref) => {
+const EmptyBasket = () => {
+  const classes = useStyles();
+
+  return (
+    <Box display="flex" height="100%">
+      <Box margin="auto">
+        <Grid direction="column" spacing={2} container>
+          <Grid className={classes.emptyBasketIcon} item>
+            <ListAltIcon />
+          </Grid>
+          <Grid item>
+            <Typography>Basket is empty</Typography>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+const ProductsBasket = forwardRef<HTMLDivElement, ProductsBasketProps>(({ onClose, ...props }, ref) => {
   const { basketItems, clearBasket } = useBasket();
   const classes = useStyles();
 
   const onSubmit = () => {
+    onClose();
 
+    //TODO: fire a notification
   };
 
   return (
-    <Dialog keepMounted TransitionComponent={Fade} scroll="paper" {...props} open>
-      <DialogTitle>Your basket</DialogTitle>
-      <DialogContent>
-        <ProductsGrid products={basketItems} />
+    <Dialog
+      keepMounted
+      TransitionComponent={Fade}
+      scroll="paper"
+      {...props}
+      classes={{
+        paper: classes.paper
+      }}
+      open
+    >
+      <DialogTitle>
+        <Typography>Your basket</Typography>
+        <IconButton onClick={onClose} className={classes.closeButton}>
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <Divider/>
+      <DialogContent className={classes.content}>
+        {basketItems.length > 0 ? <ProductsGrid products={basketItems} ProductComponent={BasketItem}/> : <EmptyBasket/>}
       </DialogContent>
+      <Divider/>
       <DialogActions>
         <Button onClick={clearBasket} className={classes.clearButton} variant="text">
           Clear
         </Button>
-        <Button onClick={onSubmit} color="primary">
-          To to checkout
+        <Button onClick={onSubmit} className={classes.submitButton}>
+          Go to checkout
         </Button>
       </DialogActions>
     </Dialog>
